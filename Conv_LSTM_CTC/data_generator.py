6746eb8037b12c1e7ffb8408b61524c2c641662d
Christian <chkaradjov@abv.bg>
2019-04-19 15:14:20 +0100

    fixing

--- Conv_LSTM_CTC/data_generator.py
+++ Conv_LSTM_CTC/data_generator.py
@@ -199,7 +199,7 @@ class DataGenerator(object):
             
             
             self._data_lists.append(decoded_audios)
-            self._labels_lists.append(np.array(label_list))
+            self._labels_lists.append(np.array(label_list, dtype=object))
         
         
         
