fb65497ef7df6a61638cec8cc3403f9058e96188
Christian <chkaradjov@abv.bg>
2019-04-28 14:21:01 +0100

    new netowrk

--- Conv_LSTM_Softmax/conv_lstm_softmax_model.py
+++ Conv_LSTM_Softmax/conv_lstm_softmax_model.py
@@ -51,11 +51,15 @@ def conv_net_part(input, batch_norm_train_mode):
                                           training=batch_norm_train_mode, conv_padding='VALID', relu=True)
         
         # Block 4
-        net = conv2d_relu_batch_norm(input=net, conv_kernel_shape=[5, 5, 256, 256], 
+        net = conv2d_relu_batch_norm(input=net, conv_kernel_shape=[7, 7, 256, 256], 
                                           training=batch_norm_train_mode, conv_padding='VALID', relu=True)
                                           
-        # Block 4
-        net = conv2d_relu_batch_norm(input=net, conv_kernel_shape=[7, 5, 256, 256], 
+        # Block 5
+        net = conv2d_relu_batch_norm(input=net, conv_kernel_shape=[7, 7, 256, 256], 
+                                          training=batch_norm_train_mode, conv_padding='VALID', relu=True)
+        
+        # Block 6
+        net = conv2d_relu_batch_norm(input=net, conv_kernel_shape=[7, 7, 256, 256], 
                                           training=batch_norm_train_mode, conv_padding='VALID', relu=True)
         
         net = tf.nn.max_pool(value=net, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
