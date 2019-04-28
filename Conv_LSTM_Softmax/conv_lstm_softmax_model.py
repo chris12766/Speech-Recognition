469b60b4fdd103d5474202e2e17071e71fb83752
Christian <chkaradjov@abv.bg>
2019-04-28 13:53:25 +0100

    new netowrk

--- Conv_LSTM_Softmax/conv_lstm_softmax_model.py
+++ Conv_LSTM_Softmax/conv_lstm_softmax_model.py
@@ -15,7 +15,8 @@ batch_size = 32
 dropout_keep_prob_train = 0.5
 
 
-def conv2d_relu_pool_batch_norm(input, conv_kernel_shape, training, conv_padding='SAME', relu=True, pool_kernel=[1, 3, 3, 1], pool_strides=[1, 2, 1, 1], pool_padding='SAME'):
+def conv2d_relu_pool_batch_norm(input, conv_kernel_shape, training, conv_padding='SAME', relu=True, 
+                                pool_kernel=[1, 2, 2, 1], pool_strides=[1, 2, 2, 1], pool_padding='SAME'):
     # create conv filter from random normal distr with mean 0 and std dev 0.01
     filter = tf.Variable(tf.truncated_normal(shape=conv_kernel_shape,
                                              stddev=0.01),
@@ -40,19 +41,19 @@ def conv_net_part(input, batch_norm_train_mode):
     
         # each conv kernel shape is [filter_height, filter_width, in_channels, out_channels]
         # Block 1
-        net = conv2d_relu_pool_batch_norm(input=input, conv_kernel_shape=[7, 7, 1, 16], 
+        net = conv2d_relu_pool_batch_norm(input=input, conv_kernel_shape=[3, 3, 1, 32], 
                                           training=batch_norm_train_mode, conv_padding='VALID', relu=True)
         
         # Block 2
-        net = conv2d_relu_pool_batch_norm(input=input, conv_kernel_shape=[5, 5, 16, 32], 
+        net = conv2d_relu_pool_batch_norm(input=input, conv_kernel_shape=[3, 3, 32, 64], 
                                           training=batch_norm_train_mode, conv_padding='VALID', relu=True)
                                           
         # Block 3
-        net = conv2d_relu_pool_batch_norm(input=input, conv_kernel_shape=[3, 3, 32, 32], 
+        net = conv2d_relu_pool_batch_norm(input=input, conv_kernel_shape=[3, 3, 64, 128], 
                                           training=batch_norm_train_mode, conv_padding='VALID', relu=True)
                                           
         # Block 4
-        net = conv2d_relu_pool_batch_norm(input=input, conv_kernel_shape=[3, 3, 32, 32], 
+        net = conv2d_relu_pool_batch_norm(input=input, conv_kernel_shape=[3, 3, 128, 256], 
                                           training=batch_norm_train_mode, conv_padding='VALID', relu=True)
     
     
